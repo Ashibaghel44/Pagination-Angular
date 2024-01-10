bddbe127973a6be48a94e855106af6c52b17d261
Ashibaghel44 <138962968+Ashibaghel44@users.noreply.github.com>
2024-01-10 13:30:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pagination';
+  data: any[] = [];
+  pageSize: number = 5;
+  paginatedData: any[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
+
+  constructor(private httpClient: HttpClient) {
+    this.loadData();
+  }
+
+  loadData(): void {
+    const apiUrl = 'https://fakestoreapi.com/products';
+
+    this.httpClient.get(apiUrl).subscribe((apiData: any) => {
+      this.data = apiData; 
+      this.updatePagination();
+    });
+  }
+
+  updatePagination(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+
+    this.paginatedData = this.data.slice(startIndex, endIndex);
+    this.totalPages = Math.ceil(this.data.length / this.pageSize);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
+  }
 }
